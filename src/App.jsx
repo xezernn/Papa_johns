a8f2367e1413e88eecc948a6a0dda05aed79f01d
xezer <xezernovruz79@gmail.com>
2023-12-07 21:16:48 +0400

    notFount, 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./Footer/Footer"
 import Header from "./Header/Header"
 import Main from "./Main/Main"
-  
+import axios from "axios"
+import { Alert, Space } from 'antd';
+
+
 function App() {
   const [sebet, setSebet] = useState(true)
   const [basket, setBasket] = useState([])
   const [basketCount, setBasketCount] = useState(0)
+  const [err, setErr] = useState("")
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get("https://papaapi.yetim.me/food")
+      .then(res => setData(res.data))
+      .catch(err => setErr(err))
+  }, [])
+
 
   return (
-    <>
+    err === "" ?
+      <>
         <Header sebet={sebet} setSebet={setSebet} />
-        <Main sebet={sebet} setSebet={setSebet} basket = {basket} setBasket={setBasket} />
+        <Main sebet={sebet} setSebet={setSebet} basket={basket} setBasket={setBasket} data={data} setData={setData} />
         <Footer />
-    </>
+      </> 
+      :
+      <div className="error">
+        <Space direction="vertical" style={{ width: '100%' }}>
+          <Alert
+            message="Error"
+            description={err.message}
+            type="error"
+            showIcon
+          />
+        </Space>
+      </div>
   )
 }
 
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,39 +4,28 @@ import Sebet from "./Sebet/Sebet"
 import Product from "./Product/Product"
 import { Route, Router, Routes } from "react-router-dom"
 import axios from "axios"
-  import Kompaniya from "./Kompaniya"
+import Kompaniya from "./Kompaniya"
 import Error404 from "./Error404"
 
 
-function Main({ sebet, setSebet, basket, setBasket}) {
+
+function Main({ sebet, setSebet, basket, setBasket,data, setData }) {
+
   
-
-  const [data, setData] = useState([])
-
-  useEffect(()=>{
-    axios.get("https://papaapi.yetim.me/food")
-    .then(res=> setData(res.data))
-  },[])
-  
-  function goo(){
-    console.log("sasasss");
-  }
-
 
   return (
-    <main>
-      <div className="container">
-        <Routes>
-          <Route path="/menu" element={<Kompaniya />} />
-          <Route path="/menu/:prop" element={<Cards basket={basket} setBasket={setBasket} data={data} setData={setData} />}/>
-          <Route path="/menu/:category/:id" element={<Product data={data} setData={setData} basket={basket} setBasket={setBasket}   />} />
-          <Route path="*" element={<Error404/>} />
-        </Routes>
-        <Sebet sebet={sebet} setSebet={setSebet} basket={basket} setBasket={setBasket} />
-   
-      </div>
+      <main>
+        <div className="container">
+          <Routes>
+            <Route path="/menu" element={<Kompaniya />} />
+            <Route path="/menu/:prop" element={<Cards basket={basket} setBasket={setBasket} data={data} setData={setData} />} />
+            <Route path="/menu/:category/:id" element={<Product data={data} setData={setData} basket={basket} setBasket={setBasket} />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+          <Sebet sebet={sebet} setSebet={setSebet} basket={basket} setBasket={setBasket} />
 
-    </main>
+        </div>
+      </main>
   )
 }
 
